ed5bc84ec8071ca609a8e5d98243a098e48b4058
Peerasak Unsakon <clonezer@gmail.com>
2023-06-19 21:40:40 +0700

    feat: update main app layout

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,45 @@
 <template>
   <div
-    class="min-h-screen h-screen bg-gray-900 text-white py-6 flex flex-col justify-center sm:py-12"
+    class="min-h-screen bg-gray-900 text-white py-6 flex flex-col justify-center sm:py-12"
   >
     <div class="relative py-3 sm:w-5/6 mx-auto w-full">
       <div class="bg-gray-800 shadow-xl sm:rounded-3xl px-4 py-10">
         <h1 class="text-4xl ml-4 lazy-font mb-6">Lazy Coder ..zzZZ</h1>
 
         <!-- Main Container -->
-        <div class="flex flex-wrap h-full">
-          <!-- Left Side Container -->
-          <div
-            :class="leftSideContainerClass"
-            class="card-container custom-scrollbar overflow-y-scroll px-4"
-          >
-            <!-- Source Code Section -->
-            <source-code-section v-model="sourceCodes"></source-code-section>
+        <div class="flex flex-col h-full">
+          <!-- Source Code Section -->
+          <div class="custom-scrollbar overflow-y-scroll px-4">
+            <source-code-section
+              v-model="sourceCodes"
+              class="mb-4"
+            ></source-code-section>
           </div>
 
-          <!-- Right Side Container -->
+          <!-- Task Section -->
           <div
-            v-show="isRightSideContainerVisible"
-            class="w-full sm:w-2/5 card-container custom-scrollbar overflow-y-scroll px-4"
+            v-if="sourceCodes.length && sourceCodesValid"
+            class="card-container custom-scrollbar overflow-y-scroll px-4"
           >
-            <!-- Task Section -->
             <task-section v-model="tasks" class="mb-4"></task-section>
+          </div>
 
-            <!-- Specialist Section -->
+          <!-- Specialist Section -->
+          <div
+            v-if="tasks.length && tasksValid"
+            class="card-container custom-scrollbar overflow-y-scroll px-4"
+          >
             <specialist-section
               v-model="selectedSpecialist"
               class="mb-4"
             ></specialist-section>
+          </div>
 
-            <!-- Final Prompt Section -->
+          <!-- Final Prompt Section -->
+          <div
+            v-if="selectedSpecialist"
+            class="card-container custom-scrollbar overflow-y-scroll px-4"
+          >
             <final-prompt-section
               :sourceCodes="sourceCodes"
               :tasks="tasks"
@@ -65,22 +73,12 @@ export default {
     };
   },
   computed: {
-    isRightSideContainerVisible() {
-      return this.sourceCodes.some(
-        (code) => code.title !== "" && code.content !== ""
-      );
+    sourceCodesValid() {
+      return this.sourceCodes.every((code) => code.title && code.content);
     },
-    leftSideContainerClass() {
-      return this.sourceCodes.length > 0 && this.isRightSideContainerVisible
-        ? "w-full sm:w-3/5"
-        : "w-full";
+    tasksValid() {
+      return this.tasks.every((task) => task.trim() !== "");
     },
   },
 };
 </script>
-
-<style scoped>
-.card-container {
-  max-height: 90vh;
-}
-</style>
