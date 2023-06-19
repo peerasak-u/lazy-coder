d448cdc593149e108d871c0fd61767494b477f34
Peerasak Unsakon <clonezer@gmail.com>
2023-06-19 15:55:37 +0700

    feat: update sepecilist section

--- src/components/SpecialistSection.vue
+++ src/components/SpecialistSection.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="my-4">
-    <h2 class="text-xl font-semibold mb-2">Specialist</h2>
+    <h2 class="text-xl font-semibold mb-2">Select Programmer</h2>
     <div>
       <label for="specialist" class="sr-only">Select a specialist</label>
+      <p class="text-gray-300 text-sm mb-2">
+        Choose a programmer who specializes in:
+      </p>
       <select
         id="specialist"
         v-model="selectedSpecialist"
         @change="$emit('update:modelValue', selectedSpecialist)"
         class="w-full mb-2 px-3 py-2 text-sm leading-tight bg-gray-600 text-gray-300 border border-gray-600 rounded shadow appearance-none focus:outline-none focus:shadow-outline"
       >
-        <option value="" disabled>Choose your master</option>
+        <option value="" disabled>Programming Language</option>
         <option value="Swift">Swift</option>
         <option value="JavaScript">JavaScript</option>
         <option value="Kotlin">Kotlin</option>
